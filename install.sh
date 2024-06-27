50a6bebbc5a994f8034e3344a8cd478a20916c60
Samson <samitesfaye726@gmail.com>
2024-06-27 21:38:39 +0300

    Bash Script For Installion

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+# setup nvim
+mv nvim/ ~/.config/
+nvim
+
+# setup tmux
+mv tmux/ ~/.config/
+tmux source-file ~/.config/tmux/tmux.conf
